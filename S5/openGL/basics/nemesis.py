249404db39641c3457f80ce51392a1e1c53fa7ad
VergilX <abhinand.dmanoj@gmail.com>
2024-02-04 20:38:33 +0530

    logic done, just fill center

--- S5/openGL/basics/nemesis.py
+++ S5/openGL/basics/nemesis.py
@@ -12,13 +12,24 @@ def init():
     glClearColor(0, 0, 0, 1)
     gluOrtho2D(-(WINDOW_SIZE//2), WINDOW_SIZE//2, -(WINDOW_SIZE//2), WINDOW_SIZE//2)
 
-def polygon(vertices: list, color: list=DEFAULT_COLOR):
+def triangle(vertices: list, color: list=DEFAULT_COLOR):
     glColor3f(*color)
 
-    glBegin(GL_POLYGON)
+    glBegin(GL_TRIANGLES)
     for vertex in vertices:
         print("vertex: ", vertex)
         glVertex2fv(vertex)
+    glEnd()
+
+def circle(center: list, radius, color: list=DEFAULT_COLOR):
+    glColor(*color)
+
+    glBegin(GL_TRIANGLE_FAN)
+    glVertex2f(*center)
+
+    for angle in range(0, 361):
+        angle_in_radian = angle * (pi/180)
+        glVertex2f(radius*cos(angle_in_radian), radius*sin(angle_in_radian))
     glEnd()
 
 
@@ -32,19 +43,20 @@ def display():
     r = int(input("Enter radius: "))
     xc, yc = int(input("Enter center x: ")), int(input("Enter center y: "))
     """
-    r = 100
+    radius = 100
     xc, yc = 0, 0
+    center = [xc, yc]
     base_angle = 360//n
     base_angle_in_rad = base_angle * (pi/180)
     # print(base_angle_in_rad)
-    print(r*sin(base_angle_in_rad), r*cos(base_angle_in_rad))
+    print(radius*sin(base_angle_in_rad), radius*cos(base_angle_in_rad))
     # print(r*sin(base_angle), r*cos(base_angle))
     
     # exit()
 
     # First triangle
-    x1, y1 = r, 0
-    x2, y2 = r*cos(base_angle_in_rad), r*sin(base_angle_in_rad)
+    x1, y1 = radius, 0
+    x2, y2 = radius*cos(base_angle_in_rad), radius*sin(base_angle_in_rad)
     # x2, y2 = r*cos(base_angle), r*sin(base_angle)
 
     x3, y3 = (x1+x2), (y1+y2)
@@ -53,11 +65,27 @@ def display():
 
     vertices.extend([
         [x1, y1],
-        [x2, y2],
-        [x3, y3]
+        [x3, y3],
+        [x2, y2]
     ])
 
-    polygon(vertices)
+    print(x3, y3)
+    # Rotating first triangle n-1 times
+    for i in range(n):
+        x1, y1 = x1*cos(base_angle_in_rad) - y1*sin(base_angle_in_rad), x1*sin(base_angle_in_rad) + y1*cos(base_angle_in_rad)
+
+        x2, y2 = x2*cos(base_angle_in_rad) - y2*sin(base_angle_in_rad), x2*sin(base_angle_in_rad) + y2*cos(base_angle_in_rad)
+
+        x3, y3 = x3*cos(base_angle_in_rad) - y3*sin(base_angle_in_rad), x3*sin(base_angle_in_rad) + y3*cos(base_angle_in_rad)
+
+        vertices.extend([
+            [x1, y1],
+            [x2, y2],
+            [x3, y3]
+        ])
+
+    triangle(vertices)
+    # circle(center, radius)
 
 
     """
@@ -70,9 +98,6 @@ def display():
         else:
             pass
     """
-
-
-    polygon(vertices)
     glutSwapBuffers()
 
 def main():
